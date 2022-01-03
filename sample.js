af377c922a828bf92087d5bea12f6b0a0f02a58c
r31mong <r31mong@yahoo.com>
2022-01-03 13:19:09 +0800

    added lines on sample.js for DevNet (45).mp4 - Commit 2

--- sample.js
+++ sample.js
@@ -10,6 +10,7 @@ myObj={    "people" : [
       {
          "firstName": "James",
          "lastName": "Smith",
+         "name": ["James","Smith"],
          "gender": "male",
          "age": 32,
          "number": "5678568567",
@@ -33,4 +34,7 @@ console.log(typeof myObj.people[1].age)
 console.log(typeof myObj.people[1].firstName)
 console.log(typeof myObj.people[1].active)
 
+
 console.log(myObj.people[0])
+console.log(myObj.people[1].name[0])
+console.log(myObj.people[1].name[1])
