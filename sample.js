c9fd493bf26733f2d38504b9e578fde36d7115ee
r31mong <r31mong@yahoo.com>
2022-01-03 12:54:40 +0800

    edited sample.js for DevNet (44).mp4

--- sample.js
+++ sample.js
@@ -4,24 +4,31 @@ myObj={    "people" : [
          "lastName": "Jackson",
          "gender": "male",
          "age": 28,
-         "number": "7349282382"
+         "number": "7349282382",
+         "active" : true
       },
       {
          "firstName": "James",
          "lastName": "Smith",
          "gender": "male",
          "age": 32,
-         "number": "5678568567"
+         "number": "5678568567",
+         "active" : false
       },
       {
          "firstName": "Emily",
          "lastName": "Jones",
          "gender": "female",
          "age": 24,
-         "number": "456754675"
+         "number": "456754675",
+         "active" : true
       }
     ]
   }
 
 console.log(myObj.people[1].lastName)
 console.log(myObj.people[1].firstName)
+
+console.log(typeof myObj.people[1].age)
+console.log(typeof myObj.people[1].firstName)
+console.log(typeof myObj.people[1].active)
