577bf614650152b34ceeb49cc08695c292663110
r31mong <r31mong@yahoo.com>
2022-01-03 12:36:48 +0800

    adding sample.js as sample javascript file

---
+++ sample.js
@@ -0,0 +1,26 @@
+myObj={    "people" : [
+      {
+         "firstName": "Joe",
+         "lastName": "Jackson",
+         "gender": "male",
+         "age": 28,
+         "number": "7349282382"
+      },
+      {
+         "firstName": "James",
+         "lastName": "Smith",
+         "gender": "male",
+         "age": 32,
+         "number": "5678568567"
+      },
+      {
+         "firstName": "Emily",
+         "lastName": "Jones",
+         "gender": "female",
+         "age": 24,
+         "number": "456754675"
+      }
+    ]
+  }
+
+console.log(myObj.people[1].lastName)
